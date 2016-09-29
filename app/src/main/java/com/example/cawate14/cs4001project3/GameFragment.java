cf46398a6c446e2724558ff58305e2499c1511f9
Charles Waters <cawate14@stlawu.edu>
2016-09-29 09:58:57 -0400

    Can dynamically add imageviews to gridlayout

--- app/src/main/java/com/example/cawate14/cs4001project3/GameFragment.java
+++ app/src/main/java/com/example/cawate14/cs4001project3/GameFragment.java
@@ -3,6 +3,7 @@ package com.example.cawate14.cs4001project3;
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.app.Fragment;
+import android.graphics.drawable.ColorDrawable;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
@@ -42,32 +43,39 @@ public class GameFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_game, container, false);
 
         GridLayout grid = (GridLayout) root.findViewById(R.id.gamegrid);
-        ImageView first = (ImageView) root.findViewById(R.id.tile0);
+        //ImageView first = (ImageView) root.findViewById(R.id.tile0);
+
+        grid.setRowCount(4);
+        grid.setColumnCount(4);
 
         // Init image views
         tileviews.clear();
-        for(int i = 1; i < 16; ++i){
+        for(int i = 0; i < 16; ++i){
             //ImageView tile = (ImageView) root.findViewById(tileids[i][j]);
             ImageView front = new ImageView(getActivity());
-            front.setLayoutParams(first.getLayoutParams());
-            front.setBackground(first.getBackground());
+            //front.setLayoutParams(first.getLayoutParams());
+            ViewGroup.LayoutParams imageParam = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            front.setLayoutParams(imageParam);
+            front.setBackground(new ColorDrawable(getResources().getColor(R.color.colorAccent)));
 
-            // This is some black magic here
-            final Animator anim = AnimatorInflater.loadAnimator(getContext(), R.animator.tile_bounce);
-            anim.setTarget(front);
-            final int x = i;
-            front.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    anim.cancel();
-                    anim.start();
-                    Log.d("DBG", "Clicked " + x);
-                }
-            });
 
-            grid.addView(front, i);
+            //final Animator anim = AnimatorInflater.loadAnimator(getContext(), R.animator.tile_bounce);
+            //anim.setTarget(front);
+            //final int x = i;
+//            front.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    anim.cancel();
+//                    anim.start();
+//                    Log.d("DBG", "Clicked " + x);
+//                }
+//            });
+
+            GridLayout.LayoutParams gridParam = new GridLayout.LayoutParams(GridLayout.spec(GridLayout.UNDEFINED, 1, 1f), GridLayout.spec(GridLayout.UNDEFINED, 1, 1f));
+            grid.addView(front, gridParam);
+
             // Need to store the references
-                tileviews.add(front);
+            tileviews.add(front);
         }
 
         return root;
